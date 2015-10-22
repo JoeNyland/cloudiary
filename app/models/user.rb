64f667d159816df7ed3bbe5516a1c21309d48cdb
Joe Nyland <joenyland@me.com>
2015-10-22 20:07:22 +0100

    Use secure passwords for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
   before_save { self.email = email.downcase }
+  has_secure_password
 end
