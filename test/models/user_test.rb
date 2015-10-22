3419a77bf032060168f9813e75375c1b6152ae9b
Joe Nyland <joenyland@me.com>
2015-10-22 08:19:19 +0100

    Make sure a user's email address is unique

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: 'Example User', email: 'user@example.com')
   end
 
   test 'should be valid' do
@@ -32,6 +32,13 @@ class UserTest < ActiveSupport::TestCase
 
   test 'email should be valid' do
     skip
+  end
+
+  test 'email addresses should be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
 end
