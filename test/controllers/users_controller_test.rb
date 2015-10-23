ea0fac83cb495c128a2ecc8b3db1cd921baf7fa3
Joe Nyland <joenyland@me.com>
2015-10-23 20:34:32 +0100

    Write some tests for Users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:joe)
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
 end
