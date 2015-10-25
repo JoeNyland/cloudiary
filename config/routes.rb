76af0f2845555901f60aee814424b304aa5f7a63
Joe Nyland <joenyland@me.com>
2015-10-25 11:26:12 +0000

    Start building login/logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root   'sessions#new'
   get    'signup'  => 'users#new'
   get    'account' => 'users#show'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
   resources :users
 
 end
