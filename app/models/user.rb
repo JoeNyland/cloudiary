fb2fa5d142b93ba25290b7fe3d192034499c4c21
Joe Nyland <joenyland@me.com>
2015-10-25 20:26:15 +0000

    Implement sessions into login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   before_save { email.downcase! }
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  # Useful to generate the digest for a a user's password in fixtures, for example.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
