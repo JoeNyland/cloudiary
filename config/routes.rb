6571b9bab15a261ee94532d76364802482fc9ad4
Joe Nyland <joenyland@me.com>
2015-10-25 20:29:19 +0000

    Add logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get    'account' => 'users#show'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 
 end
