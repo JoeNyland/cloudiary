513791b5bd214c20cb1e23fdf887c841f1280d12
Joe Nyland <joenyland@me.com>
2015-11-03 08:23:48 +0000

    Tests for Diary model

--- test/models/diary_test.rb
+++ test/models/diary_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class DiaryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:joe)
+    @diary = @user.diaries.new(title: 'My test diary')
+  end
+
+  test 'should be valid' do
+    assert @diary.valid?
+  end
+
+  test 'title must be present' do
+    @diary.title = '   '
+    assert_not @diary.valid?
+  end
+
+  test 'title must not be too long' do
+    @diary.title = 'a' * 50
+    assert_not @diary.valid?
+  end
+
 end
