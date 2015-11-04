c8f084cf569830b92be598e3823aa794dea2296f
Joe Nyland <joenyland@me.com>
2015-11-04 07:51:19 +0000

    Move logged_in_user method to ApplicationController class for use across controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user,   only: [:show, :edit, :update]
 
@@ -44,15 +45,6 @@ class UsersController < ApplicationController
   end
 
   # Before filters
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
-  end
 
   # Confirms the correct user.
   def correct_user
