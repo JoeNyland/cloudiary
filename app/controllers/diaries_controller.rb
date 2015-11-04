0a62012b8193eb8f987a363786b1b96ef3de925c
Joe Nyland <joenyland@me.com>
2015-11-04 08:05:18 +0000

    Allow creation of a diary

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,2 +1,27 @@
 class DiariesController < ApplicationController
+
+  before_action :logged_in_user
+
+  def new
+    @diary = @current_user.diaries.new
+  end
+
+  def create
+    @user = User.find_by_id(params[:user_id])
+    @diary = @user.diaries.new(diary_params)
+    if @diary.save
+      flash[:info] = 'Successfully created a diary'
+      redirect_to user_url(id:@current_user)
+    else
+      flash[:error] = 'There was a problem with the information that you submitted'
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:title)
+  end
+
 end
--- test/controllers/diaries_controller_test.rb
+++ test/controllers/diaries_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class DiariesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:joe)
+    @other_user = users(:user_1)
+  end
+
+  test 'should redirect get new when not logged in' do
+    get :new, user_id: @user
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when not logged in' do
+    # get :edit, user_id: @user
+    # assert_not flash.empty?
+    # assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    # patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    # assert_not flash.empty?
+    # assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    # log_in_as(@other_user)
+    # get :edit, id: @user
+    # assert flash.empty?
+    # assert_redirected_to root_url
+  end
 end
