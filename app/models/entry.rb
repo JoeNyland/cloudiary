79b42eea2d786ca3f23a4ce4411c5fe8f3db2e3a
Joe Nyland <joenyland@me.com>
2015-11-07 11:56:40 +0000

    Stud out the Entry model

---
+++ app/models/entry.rb
@@ -0,0 +1,5 @@
+class Entry < ActiveRecord::Base
+  belongs_to :diary
+  validates :title, presence: true, length: { maximum: 32 }
+  validates :body, presence: true, length: {minimum: 1, maximum: 500 }
+end
