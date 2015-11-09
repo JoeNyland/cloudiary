dedcf8a57abc4ee4e2ad5b9c23c9f9042daac669
Joe Nyland <joenyland@me.com>
2015-11-09 08:20:39 +0000

    Revert "Move a before filter to the application controller as it's used on pretty much all controllers"
    This reverts commit 527d4c24d746db8c32852b8e783e72e6d0f39f65.

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -59,4 +59,12 @@ class DiariesController < ApplicationController
     params.require(:diary).permit(:title)
   end
 
+  # Before filters
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
 end
