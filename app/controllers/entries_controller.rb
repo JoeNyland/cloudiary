f6250141a260d246c8297ebfd54db3dbd9423bae
Joe Nyland <joenyland@me.com>
2015-11-09 20:42:10 +0000

    Allow the creation of diary entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,45 @@
 class EntriesController < ApplicationController
+
+  before_action :logged_in_user
+  before_action :correct_user
+
+  def new
+    @user = User.find_by_id(params[:user_id])
+    @diary = @user.diaries.find_by_id(params[:diary_id])
+    @entry = @diary.entries.new
+  end
+
+  def create
+    @user = User.find_by_id(params[:user_id])
+    @diary = @user.diaries.find_by_id(params[:diary_id])
+    @entry = @diary.entries.new(entry_params)
+    if @entry.save
+      flash[:info] = 'Successfully created a diary entry'
+      redirect_to [@user,@diary,@entry]
+    else
+      flash[:error] = 'There was a problem with the information that you submitted'
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by_id(params[:user_id])
+    @diary = @user.diaries.find_by_id(params[:diary_id])
+    @entry = @diary.entries.find_by_id(params[:id])
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title,:body)
+  end
+
+  # Before filters
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
 end
