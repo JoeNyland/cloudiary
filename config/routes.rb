a54e2c9e166c9e4c29d176af2ffdba1f8fef397c
Joe Nyland <joenyland@me.com>
2015-11-09 08:23:40 +0000

    Create routes for diary entries

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users do
-    resources :diaries
+    resources :diaries do
+      resources :entries
+    end
   end
 
 end
