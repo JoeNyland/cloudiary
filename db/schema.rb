b0effab3f97b1592d96b93772408d0e596b55dcb
Joe Nyland <joenyland@me.com>
2015-11-16 08:43:04 +0000

    Remove "Owner" column from diaries table as it's not used

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107115325) do
+ActiveRecord::Schema.define(version: 20151116084138) do
 
   create_table "diaries", force: :cascade do |t|
     t.string   "title"
