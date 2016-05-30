3fae325c6f12a993eb163b8941446f8b270ae3df
Joe Nyland <joenyland@me.com>
2016-05-30 11:24:17 +0100

    Attempt to resolve db:migrate error on Heroku

--- db/migrate/20151031194449_create_diaries.rb
+++ db/migrate/20151031194449_create_diaries.rb
@@ -2,8 +2,6 @@ class CreateDiaries < ActiveRecord::Migration
   def change
     create_table :diaries do |t|
       t.text :body
-      t.references :owner, index: true, foreign_key: true
-
       t.timestamps null: false
     end
   end
--- db/migrate/20151116084138_remove_owner_from_diaries.rb
+++
@@ -1,6 +0,0 @@
-class RemoveOwnerFromDiaries < ActiveRecord::Migration
-  def change
-    remove_column :diaries, :owner, :reference
-    remove_column :diaries, :owner_id, :integer
-  end
-end
