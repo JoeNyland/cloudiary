e6a93090a7bcb1f2787fd71ee8fe5a7098f86302
Joe Nyland <joenyland@me.com>
2016-05-30 12:06:58 +0100

    Add link to sign up page in not-logged-in error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  include ActionView::Helpers::UrlHelper
 
   private
 
@@ -10,7 +11,7 @@ class ApplicationController < ActionController::Base
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = 'Please log in.'
+      flash[:danger] = "Please log in.<br>If you don't already have an account, #{link_to('click here', new_user_path).html_safe} to get started!"
       redirect_to login_url
     end
   end
